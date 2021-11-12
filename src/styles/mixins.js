bab993916fee87aaffd00577453a56f37a6a1484
unknown <rajib.mandal@sbr-technologies.com>
2021-11-12 15:23:13 +0530

    Change the font scale

--- src/styles/mixins.js
+++ src/styles/mixins.js
@@ -1,11 +1,23 @@
-import { Dimensions, PixelRatio } from 'react-native';
+import { Dimensions, PixelRatio, Platform } from 'react-native';
 
 const WINDOW_WIDTH = Dimensions.get('window').width;
+
 const guidelineBaseWidth = 375;
+const scale = WINDOW_WIDTH / guidelineBaseWidth;
 
-export const scaleSize = (size) => (WINDOW_WIDTH / guidelineBaseWidth) * size;
+export const scaleSize = (size) => scale * size;
 
-export const scaleFont = (size) => size * PixelRatio.getFontScale();
+export const scaleFontSize = (size) => size * PixelRatio.getFontScale(); //Used if want to chenge the font size according to the settings
+
+//Used this if want to set the same size of font for all screens
+export const scaleFont = (size) => {
+  const newSize = scale * size;
+  if (Platform.OS === 'ios') {
+    return Math.round(PixelRatio.roundToNearestPixel(newSize));
+  } else {
+    return Math.round(PixelRatio.roundToNearestPixel(newSize)) - 2;
+  }
+};
 
 function dimensions(top, right = top, bottom = top, left = right, property) {
   let styles = {};
